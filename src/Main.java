65ab3a37379af0fcbb4b5a1b679f2d294546359b
qqkillua <n20829187@gmail.com>
2026-09-02 23:40:28 +0500

    Домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    // Задание 1
+    System.out.println("Имя: Никита; Возраст: 19 лет; Город: Караганда");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    // Задание 2
+    int age = 19;
+    double PI = 3.14;
+    boolean truth = true;
+    String name = "Nikita";
+    char letter = 'A';
+
+    // Задание 3
+
+    for (int i = 1; i < 11; i++) {
+        System.out.println("7 * " + i + " " + "=" + " " + + 7 * i);
+    }
+
+    System.out.println();
+
+    // Задание 4
+    int size = 5;
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < 13; col++) {
+            boolean u1 = (col == 0 && row < 4) || (col == 2 && row < 4) || (row == 4 && col == 1);
+
+            boolean w = (col == 4 && row < 4) || (col == 8 && row < 4) || (row == 4 && (col == 5 || col == 7)) || (row == 3 && col == 11);
+
+            boolean u2 = (col == 10 && row < 4) || (col == 12 && row < 4) || (row == 4 && col == 11);
+
+            if (u1 || w || u2) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
     }
 }
